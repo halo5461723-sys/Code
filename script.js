a99aedbaf6f09351a4b09d942718136dab9a9f9e
halo5461723-sys <halo5461723@gmail.com>
2026-03-10 15:04:27 +0700

    Phần mềm script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Biến lưu dữ liệu
+const monsters = ["Bù Nhìn Rơm", "Người Xương", "Vua Xương", "Shadow"];
+let index = 0;
 
+// Gán sự kiện nút bấm
+document.getElementById('btn-next').addEventListener('click', function() {
+    index = (index + 1) % monsters.length;
+    document.getElementById('n-name').innerText = monsters[index];
+    console.log("Đã đổi quái thành: " + monsters[index]);
+});
